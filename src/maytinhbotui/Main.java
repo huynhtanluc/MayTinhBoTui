6450d1d044ad36bf9a262ac2ea2675714922be4c
HuynhTanLuc <luchuynhtanct@gmail.com>
2022-02-17 01:18:45 +0700

    MayTinhBoTui

---
+++ src/maytinhbotui/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package maytinhbotui;
+import maytinhbotui.MayTinhBoTui;
+/**
+ *
+ * @author luchu
+ */
+public class Main {
+    public static void main(String[] args) {
+        // Khai bao doi tuong
+        MayTinhBoTui maytinhbotui = new MayTinhBoTui();
+        maytinhbotui.show();
+    }
+}
